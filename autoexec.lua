ba59d02cbe344bf010b0d8a40b9c0f060818e84e
Mika Saarinen <mika.saarinen@gmail.com>
2013-06-16 14:24:48 +0200

    ... man should not be without best friend ;)

--- autoexec.lua
+++ autoexec.lua
@@ -35,11 +35,24 @@ function make_human()
   return t
 end
 
+function make_dog()
+  local u = ov.unit_new();
+  ov.unit_set_skeleton(u, ov.skeleton_load("dog/tr_mo_chorani.iqe"));
+  ov.unit_add_animation(u, ov.animation_load("dog/tr_mo_chien_idle.iqe"), "IDLE");
+  ov.unit_add_animation(u, ov.animation_load("dog/tr_mo_chien_marche.iqe"), "WALK");
+  ov.unit_add_skin_component(u, ov.model_load("dog/tr_mo_chorani.iqe"));
+  local t = {unit=u, action='IDLE'}
+  table.insert(unit_list, t)
+  return t
+end
+
 h1 = make_human()
 h2 = make_human()
+d1 = make_dog()
 
 ov.unit_set_position(h1.unit, 0, 0, 0)
 ov.unit_set_position(h2.unit, 2, 0, 0)
+ov.unit_set_position(d1.unit, 1, 0, 0)
 
 current_unit = 1
 
