e4fa797acbb0cd50aa576e48397e96af86b2e443
Mika Saarinen <mika.saarinen@gmail.com>
2013-10-25 23:37:47 +0200

    Change to new OO format

--- human.lua
+++ human.lua
@@ -14,3 +14,26 @@ function make_human()
   u:add_bone_component("human/fy_wea_grand_bouclier.iqe", "box_bouclier")
   return u
 end
+
+local humon_data = {
+  skeleton = "human/ge_hom_skel.iqe",
+  animations = {
+    WALK = "human/fy_hom_ab_marche.iqe",
+    IDLE = "human/fy_hom_ab_idle.iqe"
+  },
+  skin_components = {
+    "human/fy_hom_armor00_armpad.iqe",
+    "human/fy_hom_armor00_bottes.iqe",
+    "human/fy_hom_armor00_gilet.iqe",
+    "human/fy_hom_armor00_hand.iqe",
+    "human/fy_hom_armor00_pantabottes.iqe",
+    "human/fy_hom_cheveux_basic01.iqe",
+    "human/fy_hom_visage.iqe",
+    "human/fy_wea_dague.iqe", "box_arme",
+    "human/fy_wea_grand_bouclier.iqe", "box_bouclier"
+  }
+}
+
+function make_human2()
+  return make_unit_with_data(humon_data)
+end
