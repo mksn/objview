223b5865e41e7b5407a0dc39530cc7942510386c
Tor Andersson <tor@ccxvii.net>
2013-10-25 23:13:14 +0200

    Clean dog.lua

--- dog.lua
+++ dog.lua
@@ -1,12 +1,3 @@
-function make_dog()
-  local u = make_unit()
-  u:set_skeleton("dog/tr_mo_chorani.iqe")
-  u:add_animation("dog/tr_mo_chien_idle.iqe", "IDLE")
-  u:add_animation("dog/tr_mo_chien_marche.iqe", "WALK")
-  u:add_skin_component("dog/tr_mo_chorani.iqe")
-  return u
-end
-
 local dog_data = {
   skeleton = "dog/tr_mo_chorani.iqe",
   animations = {
@@ -18,6 +9,6 @@ local dog_data = {
   }
 }
 
-function make_dog2()
+function make_dog()
   return make_unit_with_data(dog_data)
 end
