461c5a2beeb08978e3ee1364d37708287e61e584
Mika Saarinen <mika.saarinen@gmail.com>
2013-11-16 00:14:46 +0100

    Added rider model

---
+++ reiter.lua
@@ -0,0 +1,22 @@
+local reiter_data = {
+  skeleton = "human/ge_hom_skel.iqe",
+  animations = {
+    WALK = "human/fy_hom_marche_in0_monture.iqe",
+    IDLE = "human/fy_hom_idle_in0_monture.iqe",
+  },
+  skin_components = {
+    "human/fy_hom_armor00_armpad.iqe",
+    "human/fy_hom_armor00_bottes.iqe",
+    "human/fy_hom_armor00_gilet.iqe",
+    "human/fy_hom_armor00_hand.iqe",
+    "human/fy_hom_armor00_pantabottes.iqe",
+    "human/fy_hom_cheveux_basic01.iqe",
+    "human/fy_hom_visage.iqe"
+  },
+}
+
+function make_reiter(parent)
+  local t = make_unit_with_data(reiter_data)
+  t:attach_model(parent,"tag.saddle")
+  return t
+end
